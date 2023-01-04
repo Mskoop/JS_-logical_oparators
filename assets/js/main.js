04e25b749ae64db4c48284f86e75e8fbd2730ffb
Msqoop <supercoder@supercoders-air.speedport.ip>
2023-01-05 00:31:09 +0100

    JS_logical

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+// AUFGABE 1.10
+
+let x = 10;
+let y = 15;
+let z = 20;
+console.log(x);
+console.log (y + 4);
+
+console.log(x > z && x < y);
+//false
+
+console.log(x != y);
+//true
+
+console.log(z < y || z > x);
+//true
+
+console.log(x == z || x !== y);
+// true
+
+console.log(x >= 10 && y <= 10);
+// false
+
+console.log(x * z < 100 || x * y > 100);
+//true
+
+
+let a = true;
+let b = false;
+console.log(a);
+console.log(b);
+
+console.log(a && b);
+//false
+
+console.log(a || b);
+//true
+
+console .log( !a && b);
+
+// AUFGABE 1.15
+
+let num = 5;
+let anotherNum = 8;
+
+console.log(num == anotherNum);
+// false
+
+console.log(num < anotherNum);
+//true
+
+console.log(num > anotherNum);
+//false
+
+console.log(num >= anotherNum);
+// false
+
+console.log(num <= anotherNum);
+//true
+
+console.log(num == "5");
+//true
+
+console.log(num != 8);
+//true
+
+console.log(num != "5");
+//false
